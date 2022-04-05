5def89be451054a1ea250421e9fd4e32af10f1f9
Max Kurko <msxkurko3@gmail.com>
2022-04-05 17:09:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿using namespace std;
+#include <iostream>
+#include <math.h>
+
+
+int main()
+{
+   	double a = 5.1;
+	double b = 1.2;
+	double c = -0.3;
+	double d = -3.1;
+	
+	double y;
+
+	double underSqrtRoot = c / d;
+	double baseLn = pow(b, a);
+
+
+	if ((underSqrtRoot > 0) && (baseLn > 0))
+	{
+		y = fabs(acos(sqrt(underSqrtRoot))) + 7 * log10(baseLn);
+		cout << y << endl;
+	}
+	else
+	{
+		cout << "error" << endl;
+	}
+}
+
